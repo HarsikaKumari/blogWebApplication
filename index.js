7df212388fdeaea4cadf4fdebf8535a908c9fb3f
Harsika Kumari <kumariharsika8@gmail.com>
2024-01-19 00:38:36 +0530

    created form and handeled the form in server

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/post", (req, res) => {
+  const postTitle = res.body["title"];
+
+  app.writeFile("tittle.txt", postTitle, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+});
+
+app.post("/submit", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`the server is running on ${port}`);
+});
